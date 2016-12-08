41360db8c444c6feb4c5af523b754dd1faabc2d0
Faseldi <florianduneaufamily@hotmail.fr>
2016-12-08 20:26:46 +0100

    changement des backgrounds des items de la vue principale

---
+++ app/src/main/java/sortiedechien/fr/search/ResultSearchActivity.java
@@ -0,0 +1,13 @@
+package sortiedechien.fr.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ResultSearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result_search);
+    }
+}
