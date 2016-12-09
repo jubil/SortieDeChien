17d4b5cc70069d313cb81d418487e3fd60490be2
Faseldi <florianduneaufamily@hotmail.fr>
2016-12-09 09:47:06 +0100

    modification de la vue de recherche et des intéractions sur celle-ci

--- app/src/main/java/sortiedechien/fr/search/AdvancedSearchActivity.java
+++ app/src/main/java/sortiedechien/fr/search/AdvancedSearchActivity.java
@@ -1,8 +1,15 @@
 package sortiedechien.fr.search;
 
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
 
+import sortiedechien.fr.sortiedechien.MainActivity;
 import sortiedechien.fr.sortiedechien.R;
 
 public class AdvancedSearchActivity extends AppCompatActivity {
@@ -11,5 +18,18 @@ public class AdvancedSearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_advanced_search);
+        Button button = (Button) findViewById(R.id.search_button);
+        button.setOnClickListener( new OnSearchLaunchClickListener(this) );
+
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#49D436")));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == android.R.id.home){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        }
+        return true;
     }
 }
