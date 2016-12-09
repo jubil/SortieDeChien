4bf2930d2da13ac4fc12408d415bc6fa10089d67
darkguichou <lechelon.guillaume@gmail.com>
2016-12-09 10:20:40 +0100

    Ajout du gestionnaire de la bdd et de la couche dao necessaire pour la classe PARC

--- app/src/main/java/sortiedechien/fr/dao/BaseDao.java
+++ app/src/main/java/sortiedechien/fr/dao/BaseDao.java
@@ -1,8 +1,46 @@
 package sortiedechien.fr.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import sortiedechien.fr.sqlite.DbHandler;
+
 /**
  * Created by guillaume on 09/12/16.
  */
 
 public class BaseDao {
+
+
+    private  final static String NAME = "sortieChien.db";
+    private final static int VERSION = 1;
+    protected SQLiteDatabase db = null;
+    protected DbHandler handler = null;
+
+
+    public  BaseDao(Context context){
+
+        this.handler = new DbHandler(context, NAME, null, VERSION);
+    }
+
+    public SQLiteDatabase open(){
+
+        db = handler.getWritableDatabase();
+
+        return db;
+
+    }
+
+
+    public void close(){
+
+        db.close();
+    }
+
+    public SQLiteDatabase getDb(){
+
+        return db;
+    }
+
+
 }
