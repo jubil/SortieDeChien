c434c062086fa273410053948c3d270a71bafc19
darkguichou <lechelon.guillaume@gmail.com>
2016-12-12 14:04:09 +0100

    ajouts

--- app/src/main/java/sortiedechien/fr/dao/ArbreDao.java
+++ app/src/main/java/sortiedechien/fr/dao/ArbreDao.java
@@ -1,8 +1,55 @@
 package sortiedechien.fr.dao;
 
+import android.content.Context;
+import android.database.Cursor;
+
+import java.util.ArrayList;
+
+import sortiedechien.fr.data.Arbre;
+import sortiedechien.fr.data.Parc;
+import sortiedechien.fr.sqlite.DbHandler;
+
 /**
- * Created by guillaume on 12/12/16.
+ * Created by guillau
+ * me on 12/12/16.
  */
 
-public class ArbreDao {
+public class ArbreDao extends BaseDao {
+
+    private String tablename = DbHandler.TABLE_ARBRE;
+    private String[] allcolumns = {
+            DbHandler.COLUMN_ID,
+            DbHandler.COLUMN_ADRESSE,
+            DbHandler.COLUMN_LONGITUDE,
+            DbHandler.COLUMN_LATTITUDE
+    };
+
+    public ArbreDao(Context context) {
+        super(context);
+    }
+
+
+    public ArrayList<Arbre> selectAll(){
+
+
+        ArrayList<Arbre> arbres = new ArrayList<Arbre>();
+        String[] params = {tablename};
+        Cursor c = super.db.rawQuery("SELECT * FROM ?", params);
+
+        while (c.moveToNext()){
+
+            arbres.add(new Arbre(c.getInt(0), c.getString(1), c.getFloat(2), c.getFloat(3)));
+
+        }
+
+        c.close();
+
+        return arbres;
+    }
+
+    public void insert(int id, String adresse, float lat, float lon){
+
+        String[] params = {tablename, ""+id, ""+adresse, ""+lat, ""+lon};
+        super.db.rawQuery("INSERT INTO ? VALUES(?, ?, ?, ?);", params);
+    }
 }
--- app/src/main/java/sortiedechien/fr/data/Arbre.java
+++ app/src/main/java/sortiedechien/fr/data/Arbre.java
@@ -5,4 +5,48 @@ package sortiedechien.fr.data;
  */
 
 public class Arbre {
+
+    private int id;
+    private String adresse;
+    private float lattitude;
+    private float longitude;
+
+    public Arbre(int id, String adresse, float lattitude, float longitude) {
+        this.id = id;
+        this.adresse = adresse;
+        this.lattitude = lattitude;
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(String adresse) {
+        this.adresse = adresse;
+    }
+
+    public float getLattitude() {
+        return lattitude;
+    }
+
+    public void setLattitude(float lattitude) {
+        this.lattitude = lattitude;
+    }
+
+    public float getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(float longitude) {
+        this.longitude = longitude;
+    }
 }
